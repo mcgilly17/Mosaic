3c15177099de962bdaf339e287540032fc93a61c
McGilly17 <4136843+mcgilly17@users.noreply.github.com>
2024-08-24 09:49:11 -0600

    test: make settings object a default to see if it can be overwritten

--- config/plugins/bufferline.nix
+++ config/plugins/bufferline.nix
@@ -2,7 +2,7 @@
   plugins = {
     bufferline = {
       enable = pkgs.lib.mkDefault true;
-      settings.options = {
+      settings.options = pkgs.lib.mkDefault {
         separatorStyle = "thick"; # “slant”, “padded_slant”, “slope”, “padded_slope”, “thick”, “thin”
         offsets = [
           {
