b2da54386bc4a2d73038698fad06c7591a273187
McGilly17 <4136843+mcgilly17@users.noreply.github.com>
2024-08-26 22:39:09 -0600

    feat: nicer lualine configuration

--- config/ui/lualine.nix
+++ config/ui/lualine.nix
@@ -20,14 +20,17 @@
         {
           name = "mode";
           separator = {
-            left = "";
+            right = "";
           };
-          padding = 2;
+          icon = "󰻀";
         }
       ];
       lualine_b = [
         {
           name = "branch";
+          separator = {
+            right = "";
+          };
           icon = "";
         }
         "diff"
@@ -35,14 +38,40 @@
       ];
       lualine_c = ["filename"];
       lualine_x = ["filetype"];
-      lualine_y = ["progress"];
+      lualine_y = [
+        {
+          name.__raw = ''
+            function() return vim.fn['getcwd']() end
+          '';
+          icon.__raw = "{' ', color={fg='#1e1e2e',bg='#f38ba8'}}";
+          separator = {
+            left = "";
+          };
+          color = {
+            fg = "#6c6f85";
+            bg = "#181825";
+          };
+          padding = {
+            right = 1;
+            left = 0;
+          };
+        }
+      ];
       lualine_z = [
         {
-          name = ''" " .. os.date("%R")'';
+          name = "progress";
+          icon.__raw = "{' ', color={fg='#1e1e2e',bg='#a6e3a1'}}";
           separator = {
-            right = "";
+            left = "";
           };
-          padding = 2;
+          color = {
+            fg = "#a6e3a1";
+            bg = "#181825";
+          };
+          padding = {
+            right = 1;
+            left = 0;
+          };
         }
       ];
     };
