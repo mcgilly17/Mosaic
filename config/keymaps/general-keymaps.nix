0746c9cf2dcccdac19eabe774c0ce26ac08a6cb0
McGilly17 <4136843+mcgilly17@users.noreply.github.com>
2024-08-30 18:20:46 -0500

    feat(refactor): Moving buffer keymaps into general and bufferline keymaps back into a decentralised model

--- config/keymaps/general-keymaps.nix
+++ config/keymaps/general-keymaps.nix
@@ -2,6 +2,39 @@ _: {
   keymaps = [
     /*
     =============================================
+    =                 Buffers                   =
+    =============================================
+    */
+
+    {
+      mode = "n";
+      key = "<leader>bd";
+      action = "<cmd>bdelete<cr>";
+      options = {
+        desc = "Delete buffer";
+      };
+    }
+
+    {
+      mode = "n";
+      key = "<leader>bD";
+      action = "<Cmd>bdelete!<CR>";
+      options = {
+        desc = "Force Delete";
+      };
+    }
+
+    {
+      mode = "n";
+      key = "<leader>bb";
+      action = "<cmd>e #<cr>";
+      options = {
+        desc = "Switch to Other Buffer";
+      };
+    }
+
+    /*
+    =============================================
     =         Movement / Disable Arrows         =
     =============================================
     */
--- config/ui/bufferline.nix
+++ config/ui/bufferline.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   plugins = {
     bufferline = {
       enable = pkgs.lib.mkDefault true;
@@ -15,4 +19,74 @@
       };
     };
   };
+
+  keymaps = pkgs.lib.mkIf config.plugins.bufferline.enable [
+    /*
+    =============================================
+    =                  Bufferline               =
+    =============================================
+    */
+
+    {
+      mode = "n";
+      key = "<Tab>";
+      action = "<cmd>BufferLineCycleNext<cr>";
+      options = {
+        desc = "Cycle to next buffer";
+      };
+    }
+
+    {
+      mode = "n";
+      key = "<S-Tab>";
+      action = "<cmd>BufferLineCyclePrev<cr>";
+      options = {
+        desc = "Cycle to previous buffer";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>br";
+      action = "<cmd>BufferLineCloseRight<cr>";
+      options = {
+        desc = "Delete buffers to the right";
+      };
+    }
+
+    {
+      mode = "n";
+      key = "<leader>bl";
+      action = "<cmd>BufferLineCloseLeft<cr>";
+      options = {
+        desc = "Delete buffers to the left";
+      };
+    }
+
+    {
+      mode = "n";
+      key = "<leader>bo";
+      action = "<cmd>BufferLineCloseOthers<cr>";
+      options = {
+        desc = "Delete other buffers";
+      };
+    }
+
+    {
+      mode = "n";
+      key = "<leader>bp";
+      action = "<cmd>BufferLineTogglePin<cr>";
+      options = {
+        desc = "Toggle pin";
+      };
+    }
+
+    {
+      mode = "n";
+      key = "<leader>bP";
+      action = "<Cmd>BufferLineGroupClose ungrouped<CR>";
+      options = {
+        desc = "Delete non-pinned buffers";
+      };
+    }
+  ];
 }
