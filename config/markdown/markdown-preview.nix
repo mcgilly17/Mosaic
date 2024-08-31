c6e4f90ff73b934247b5ade2ae55d3f89b92e5b7
McGilly17 <4136843+mcgilly17@users.noreply.github.com>
2024-08-30 21:56:33 -0500

    feat(refactor): Move Markdown keymaps to plugin files

--- config/markdown/markdown-preview.nix
+++ config/markdown/markdown-preview.nix
@@ -1,8 +1,23 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   plugins.markdown-preview = {
     enable = pkgs.lib.mkDefault true;
     settings = {
       theme = pkgs.lib.mkDefault "dark";
     };
   };
+
+  keymaps = pkgs.lib.mkIf config.plugins.markdown-preview.enable [
+    {
+      mode = "n";
+      key = "<leader>mp";
+      action = "<cmd>MarkdownPreview<cr>";
+      options = {
+        desc = "Markdown Preview";
+      };
+    }
+  ];
 }
