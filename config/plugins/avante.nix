5a486f3843ae8325639e7208859a1c1423b10c37
McGilly17 <4136843+mcgilly17@users.noreply.github.com>
2024-10-21 11:58:57 -0600

    udpate: avante which key

--- config/plugins/avante.nix
+++ config/plugins/avante.nix
@@ -1,8 +1,24 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: {
   plugins.avante = {
     enable = pkgs.lib.mkDefault true;
     settings = {
       provider = "openai";
     };
   };
+  plugins.which-key.settings.spec = lib.optionals config.plugins.avante.enable [
+    {
+      __unkeyed-1 = "<leader>a";
+      mode = [
+        "n"
+        "v"
+      ];
+      group = "+Co-Pilot";
+      icon = "";
+    }
+  ];
 }
