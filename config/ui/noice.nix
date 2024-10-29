4e6f0cdab6c300275f55d848e0171cebf6df03f0
McGilly17 <4136843+mcgilly17@users.noreply.github.com>
2024-10-29 10:48:56 -0600

    chore: settings fix for noice after flake update

--- config/ui/noice.nix
+++ config/ui/noice.nix
@@ -1,38 +1,40 @@
 {pkgs, ...}: {
   plugins.noice = {
     enable = pkgs.lib.mkDefault true;
-    notify = {
-      enabled = false;
-    };
-    messages = {
-      enabled = true; # Adds a padding-bottom to neovim statusline when set to false for some reason
-    };
-    lsp = {
-      message = {
-        enabled = true;
-      };
-      progress = {
+    settings = {
+      notify = {
         enabled = false;
-        view = "mini";
       };
-    };
-    presets = {
-      inc_rename = true;
-    };
-    popupmenu = {
-      enabled = true;
-      backend = "nui";
-    };
-    format = {
-      filter = {
-        pattern = [":%s*%%s*s:%s*" ":%s*%%s*s!%s*" ":%s*%%s*s/%s*" "%s*s:%s*" ":%s*s!%s*" ":%s*s/%s*"];
-        icon = "";
-        lang = "regex";
+      messages = {
+        enabled = true; # Adds a padding-bottom to neovim statusline when set to false for some reason
       };
-      replace = {
-        pattern = [":%s*%%s*s:%w*:%s*" ":%s*%%s*s!%w*!%s*" ":%s*%%s*s/%w*/%s*" "%s*s:%w*:%s*" ":%s*s!%w*!%s*" ":%s*s/%w*/%s*"];
-        icon = "󱞪";
-        lang = "regex";
+      lsp = {
+        message = {
+          enabled = true;
+        };
+        progress = {
+          enabled = false;
+          view = "mini";
+        };
+      };
+      presets = {
+        inc_rename = true;
+      };
+      popupmenu = {
+        enabled = true;
+        backend = "nui";
+      };
+      format = {
+        filter = {
+          pattern = [":%s*%%s*s:%s*" ":%s*%%s*s!%s*" ":%s*%%s*s/%s*" "%s*s:%s*" ":%s*s!%s*" ":%s*s/%s*"];
+          icon = "";
+          lang = "regex";
+        };
+        replace = {
+          pattern = [":%s*%%s*s:%w*:%s*" ":%s*%%s*s!%w*!%s*" ":%s*%%s*s/%w*/%s*" "%s*s:%w*:%s*" ":%s*s!%w*!%s*" ":%s*s/%w*/%s*"];
+          icon = "󱞪";
+          lang = "regex";
+        };
       };
     };
   };
