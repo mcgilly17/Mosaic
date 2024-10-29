20ff4fe50a10ee9c5dc67fb43b9eb99d587e3961
McGilly17 <4136843+mcgilly17@users.noreply.github.com>
2024-10-29 10:42:21 -0600

    chore: flake update with config updates
    Signed-off-by: McGilly17 <4136843+mcgilly17@users.noreply.github.com>

--- config/ui/nui.nix
+++ config/ui/nui.nix
@@ -1,14 +1,5 @@
-{
-  lib,
-  pkgs,
-  config,
-  ...
-}: let
-  cfg = config.plugins.nui;
-in {
-  options.plugins.nui.enable = lib.mkEnableOption "nui setup";
-
-  config = lib.mkIf cfg.enable {
-    extraPlugins = with pkgs.vimPlugins; [nui-nvim];
+{pkgs, ...}: {
+  plugins.nui = {
+    enable = pkgs.lib.mkDefault true;
   };
 }
