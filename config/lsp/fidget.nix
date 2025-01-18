cbf89621adc6c84165044a6a39d4460083671455
McGilly17 <4136843+mcgilly17@users.noreply.github.com>
2025-01-18 13:39:46 -0800

    fix: update names for fidget

--- config/lsp/fidget.nix
+++ config/lsp/fidget.nix
@@ -1,19 +1,21 @@
 {pkgs, ...}: {
   plugins.fidget = {
     enable = pkgs.lib.mkDefault true;
-    logger = pkgs.lib.mkDefault {
-      level = "warn"; # “off”, “error”, “warn”, “info”, “debug”, “trace”
-      floatPrecision = 0.01; # Limit the number of decimals displayed for floats
-    };
-    progress = pkgs.lib.mkDefault {
-      suppressOnInsert = true; # Suppress new messages while in insert mode
-    };
-    notification = pkgs.lib.mkDefault {
-      overrideVimNotify = true;
+    settings = {
+      logger = pkgs.lib.mkDefault {
+        level = "warn"; # “off”, “error”, “warn”, “info”, “debug”, “trace”
+        float_precision = 0.01; # Limit the number of decimals displayed for floats
+      };
+      progress = pkgs.lib.mkDefault {
+        suppress_on_insert = true; # Suppress new messages while in insert mode
+      };
+      notification = pkgs.lib.mkDefault {
+        override_vim_notify = true;
 
-      window = {
-        winblend = 0;
-        border = "none"; # none, single, double, rounded, solid, shadow
+        window = {
+          winblend = 0;
+          border = "none"; # none, single, double, rounded, solid, shadow
+        };
       };
     };
   };
